1ff8c315c86baf7aba32d0f9592e62ea064503e8
Ping R. <witcha.rath@gmail.com>
2024-12-20 18:21:26 +0700

    optimize table re-render

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { FC, useEffect, useState } from 'react';
 import { fetchStockRsList } from './services/us-rs-screener.service';
 import { Stock } from './models/stock';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Skeleton } from '@chakra-ui/react';
 import { DataTable } from './components/app/data-table';
 import { Settings } from './components/app/settings';
 import { defaultSettings } from './utils/constants';
 import { initialFilter } from './utils/table.util';
 import { useEventListener } from 'usehooks-ts';
+import { If } from './components/ui/if';
 
 const App: FC = () => {
   const [stockList, setStockList] = useState<Stock[]>([]);
@@ -48,11 +49,19 @@ const App: FC = () => {
       <Heading size="3xl" paddingY={2} paddingX={1}>
         US Stock Screener
       </Heading>
-      <DataTable
-        data={filteredStockList}
-        settings={[
-          <Settings key={'app-settings'} currentSettings={settings} saveSettings={setSettings} />
-        ]}></DataTable>
+      <If exp={filteredStockList.length > 0}>
+        <DataTable
+          data={filteredStockList}
+          settings={[
+            <Settings key={'app-settings'} currentSettings={settings} saveSettings={setSettings} />
+          ]}></DataTable>
+      </If>
+      <If exp={filteredStockList.length === 0}>
+        <Skeleton flex="1" height="4" variant="pulse" marginY={4} />
+        <Skeleton flex="1" height="4" variant="pulse" marginY={4} />
+        <Skeleton flex="1" height="4" variant="pulse" marginY={4} />
+        <Skeleton flex="1" height="4" variant="pulse" marginY={4} />
+      </If>
     </Box>
   );
 };

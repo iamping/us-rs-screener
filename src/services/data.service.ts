3daa4211affa41cbc3d77a01ec3413a6875257a7
Ping R. <witcha.rath@gmail.com>
2025-01-01 22:08:49 +0700

    refactor data service

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -5,34 +5,30 @@ const cacheKey = 'historical-' + new Date().toISOString().substring(0, 10);
 
 export const fetchStockRsList = async (): Promise<Stock[]> => {
   const url = './api/us_rs_list.json';
-  const cache = await caches.open(cacheKey);
-  const cacheResponse = await cache.match(url);
-  if (cacheResponse) {
-    return cacheResponse.json();
-  }
-
-  await clearCache([cacheKey]);
-
-  const response = await fetch(url);
-  cache.put(url, response.clone());
-  return response.clone().json();
+  return await fetchOrRetrieve(url, cacheKey);
 };
 
 export const fetchHistoricalData = async (ticker: string): Promise<HistoricalData> => {
   const params = new URLSearchParams({ ticker: ticker }).toString();
   const path = import.meta.env.DEV ? './historical-api' : 'https://yf-proxy.koyeb.app/';
   const url = `${path}?${params}`;
-  const cache = await caches.open(cacheKey);
-  const cacheResponse = await cache.match(url);
-  if (cacheResponse) {
-    return cacheResponse.json();
+  return await fetchOrRetrieve(url, cacheKey);
+};
+
+const fetchOrRetrieve = async (url: string, cacheKey: string) => {
+  if (window.caches) {
+    const cache = await window.caches.open(cacheKey);
+    const cacheResponse = await cache.match(url);
+    if (cacheResponse) {
+      return cacheResponse.json();
+    }
+    await clearCache([cacheKey]);
+    const response = await fetch(url);
+    cache.put(url, response.clone());
+    return response.clone().json();
+  } else {
+    return fetch(url).then((response) => response.clone().json());
   }
-
-  await clearCache([cacheKey]);
-
-  const response = await fetch(url);
-  cache.put(url, response.clone());
-  return response.clone().json();
 };
 
 const clearCache = async (excludeKeys?: string[]) => {

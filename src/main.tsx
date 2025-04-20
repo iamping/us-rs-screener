bf4ff56a04a32473898d1d32d3b6fa9ed7336065
Ping R. <witcha.rath@gmail.com>
2025-04-20 22:37:11 +0700

    refactor: move files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
-import './main.css';
+import './styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

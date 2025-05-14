e60f7d9aa07814aea9200938151a8564c2fa4f01
Ping R. <witcha.rath@gmail.com>
2025-05-14 15:52:43 +0700

    refactor Canvas component

--- src/components/stock-chart/canvas.tsx
+++ src/components/stock-chart/canvas.tsx
@@ -1,33 +1,55 @@
-import { CSSProperties, FC, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef } from 'react';
 
 interface CanvasProps extends React.HTMLAttributes<HTMLCanvasElement> {
-  draw?: (context: CanvasRenderingContext2D) => void;
+  draw: (context: CanvasRenderingContext2D) => void;
 }
 
-export const Canvas: FC<CanvasProps> = ({ draw, style, ...rest }) => {
+export const Canvas: FC<CanvasProps> = ({ draw, ...rest }) => {
   const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries = []) => {
+      entries.forEach((entry) => {
+        const { width, height } = entry.contentRect;
+        const pixelRatio = devicePixelRatio || 1;
+        const canvas = ref.current as HTMLCanvasElement;
+        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        if (entry.devicePixelContentBoxSize) {
+          const { inlineSize, blockSize } = entry.devicePixelContentBoxSize[0];
+          canvas.width = inlineSize;
+          canvas.height = blockSize;
+        } else {
+          canvas.width = width * pixelRatio;
+          canvas.height = height * pixelRatio;
+        }
+        render(context, draw);
+      });
+    });
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [draw]);
 
   useEffect(() => {
     const canvas = ref.current as HTMLCanvasElement;
     const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-    const render = () => {
-      const { width, height } = style as CSSProperties;
-      predraw(context, width as number, height as number);
-      if (draw) {
-        draw(context);
-      }
-      postdraw(context);
-    };
-    render();
-  }, [ref, draw, style]);
+    render(context, draw);
+  }, [draw]);
 
-  return <canvas ref={ref} style={style} {...rest}></canvas>;
+  return <canvas ref={ref} {...rest}></canvas>;
 };
 
-const predraw = (context: CanvasRenderingContext2D, width: number, height: number) => {
+const render = (context: CanvasRenderingContext2D, draw: (context: CanvasRenderingContext2D) => void) => {
+  predraw(context);
+  draw(context);
+  postdraw(context);
+};
+
+const predraw = (context: CanvasRenderingContext2D) => {
   const canvas = context.canvas;
-  canvas.width = Math.round(width * devicePixelRatio);
-  canvas.height = Math.round(height * devicePixelRatio);
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.save();
 };

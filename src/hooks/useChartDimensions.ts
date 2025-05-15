bd60e84fe680f97fb0178260f60107e542824705
Ping R. <witcha.rath@gmail.com>
2025-05-15 17:45:15 +0700

    refactor useChartDimensions

--- src/hooks/useChartDimensions.ts
+++ src/hooks/useChartDimensions.ts
@@ -23,37 +23,45 @@ export interface ChartDimensions {
 export const useChartDimensions = <T extends HTMLElement>(
   margins?: ChartMargins
 ): [RefObject<T | null>, ChartDimensions] => {
-  const ref = useRef<T>(null);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [diffWidth, setDiffWidth] = useState(0);
-
   const plotMargins = {
     marginTop: margins?.marginTop || 0,
     marginRight: margins?.marginRight || 0,
     marginBottom: margins?.marginBottom || 0,
     marginLeft: margins?.marginLeft || 0
   };
+  const ref = useRef<T>(null);
+  const [dms, setDimensions] = useState<ChartDimensions>({
+    marginTop: plotMargins.marginTop,
+    marginRight: plotMargins.marginRight,
+    marginBottom: plotMargins.marginBottom,
+    marginLeft: plotMargins.marginLeft,
+    height: 0,
+    width: 0,
+    diffWidth: 0,
+    plotHeight: 0,
+    plotWidth: 0
+  });
 
   const onResize = useDebounceCallback((size) => {
-    if (Math.round(size.width) !== Math.round(width) || Math.round(size.height) !== Math.round(height)) {
-      setWidth(Math.round(size.width));
-      setHeight(Math.round(size.height));
-      setDiffWidth(Math.round(size.width) - Math.round(width));
+    if (Math.round(size.width) !== Math.round(dms.width) || Math.round(size.height) !== Math.round(dms.height)) {
+      const width = Math.round(size.width);
+      const height = Math.round(size.height);
+      setDimensions({
+        marginTop: margins?.marginTop || 0,
+        marginRight: margins?.marginRight || 0,
+        marginBottom: margins?.marginBottom || 0,
+        marginLeft: margins?.marginLeft || 0,
+        width: width,
+        height: height,
+        diffWidth: width - Math.round(dms.width),
+        plotHeight: Math.max(height - plotMargins.marginTop - plotMargins.marginBottom, 0),
+        plotWidth: Math.max(width - plotMargins.marginLeft - plotMargins.marginRight, 0)
+      });
     }
   }, 0);
 
   const resizeRef = ref as RefObject<HTMLElement>;
   useResizeObserver({ ref: resizeRef, box: 'border-box', onResize });
 
-  const updatedDimensions: ChartDimensions = {
-    ...plotMargins,
-    height: height,
-    width: width,
-    diffWidth: diffWidth,
-    plotHeight: Math.max(height - plotMargins.marginTop - plotMargins.marginBottom, 0),
-    plotWidth: Math.max(width - plotMargins.marginLeft - plotMargins.marginRight, 0)
-  };
-
-  return [ref, updatedDimensions] as const;
+  return [ref, dms] as const;
 };
